501179c27dc3596f88a4fb44bdc2dda48052dec9
Kevin Solorio <kevin@detone8.com>
2014-06-24 00:06:04 -0500

    initial outline of testing scenarios

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Log In" do
+
+  context "with valid credentials" do
+    it "signs me in"
+  end
+
+  context "with invalid credentials" do
+    it "does not sign me in"
+  end
+
+end
