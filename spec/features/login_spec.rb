57343e1a050c8f00bea588a3f3832fd515647f0e
Kevin Solorio <kevin@detone8.com>
2014-06-24 01:08:01 -0500

    update feature for implementation

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,13 +1,25 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe "Log In" do
+feature 'Login In' do
 
-  context "with valid credentials" do
-    it "signs me in"
+  context 'with valid credentials' do
+    scenario 'signs me in' do
+      # Given I'm on the login page
+      visit '/sessions/new'
+      # And I have filled in the form with valid credentials
+      within('#authenticate') do
+        fill_in 'Username', with: 'ksolo'
+        fill_in 'Password', with: '123456'
+      end
+      # When I click the login button
+      click_on 'Login'
+      # Then I should be on a page that says welcome back
+      expect(page).to have_text('Welcome Back')
+    end
   end
 
   context "with invalid credentials" do
-    it "does not sign me in"
+    scenario "does not sign me in"
   end
 
 end
